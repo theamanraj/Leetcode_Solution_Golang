efb3e09b223efd93b53e814de5617f2e01fb4842
Aman <33025632+theamanraj@users.noreply.github.com>
2022-11-26 09:43:42 +0530

    Time: 37 ms (85.26%), Space: 7 MB (56.37%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.go
@@ -0,0 +1,16 @@
+func search(nums []int, target int) int {
+    l,h := 0,len(nums)-1
+    for l <= h{
+        m := (l+h)/2
+                if (nums[m] == target){
+            return m
+        } 
+        if (nums[m] < target){
+            l = m+1
+        }
+                if (nums[m] > target){
+            h = m-1
+        }
+    }
+    return -1
+}
