33943849710e57bebe4c681e101b2baed8a41c83
Aman <33025632+theamanraj@users.noreply.github.com>
2022-11-26 09:31:20 +0530

    Time: 4 ms (38.08%), Space: 2.1 MB (76.15%) - LeetHub

---
+++ 0263-ugly-number/0263-ugly-number.go
@@ -0,0 +1,15 @@
+func isUgly(n int) bool {
+    if n <= 0 {
+        return false
+    }
+    for n % 2 == 0 {
+        n /= 2
+    }
+    for n % 3 == 0 {
+        n /= 3
+    }
+    for n % 5 == 0 {
+        n /= 5
+    }
+    return n == 1 
+}
