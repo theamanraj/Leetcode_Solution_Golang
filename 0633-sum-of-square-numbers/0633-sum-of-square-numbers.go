f1b48bf052dacdd096b6cc972d240299c845e938
Aman <33025632+theamanraj@users.noreply.github.com>
2022-12-05 21:24:31 +0530

    Time: 0 ms (100.00%), Space: 1.9 MB (28.30%) - LeetHub

---
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.go
@@ -0,0 +1,10 @@
+func judgeSquareSum(c int) bool {
+	for i := 0; i*i <= c; i++ {
+		temp := c - i*i
+		float_c := int(math.Sqrt(float64(temp)))
+		if float_c*float_c+i*i == c {
+			return true
+		}
+	}
+	return false
+}
