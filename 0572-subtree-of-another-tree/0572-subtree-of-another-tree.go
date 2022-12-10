66589abfff073a67f663fad94a3566dc1d65624c
Aman <33025632+theamanraj@users.noreply.github.com>
2022-12-10 09:29:29 +0530

    Time: 7 ms (99.68%), Space: 6.5 MB (88.31%) - LeetHub

---
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.go
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func isSubtree(s, t *TreeNode) bool {
+	if t == nil {
+		return true
+	}
+	if s == nil {
+		return false
+	}
+	return isSame(s, t) || isSubtree(s.Left, t) || isSubtree(s.Right, t)
+}
+
+func isSame(s, t *TreeNode) bool {
+	if s == nil && t == nil {
+		return true
+	}
+	if s == nil || t == nil {
+		return false
+	}
+	return s.Val == t.Val && isSame(s.Left, t.Left) && isSame(s.Right, t.Right)
+}
