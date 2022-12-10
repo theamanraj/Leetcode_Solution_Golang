2a4de432bc8319d0f94f82d9a17e6eda105387b1
Aman <33025632+theamanraj@users.noreply.github.com>
2022-12-10 09:27:57 +0530

    Time: 7 ms (91.87%), Space: 5.2 MB (56.66%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.go
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.go
@@ -1,50 +1,48 @@
-type myChars []byte
+import "sort"
 
-func (inData myChars) Less(i int, j int) bool {
-        return (inData[i] < inData[j])
+type String []rune
+
+func (s String) Len() int           { return len(s) }
+func (s String) Less(i, j int) bool { return s[i] < s[j] }
+func (s String) Swap(i, j int)      { s[i], s[j] = s[j], s[i] }
+
+func sortString(inp string) string {
+	r := []rune(inp)
+	sort.Sort(String(r))
+	return string(r)
 }
 
-func (inData myChars) Swap(i int, j int) {
-        inData[i], inData[j] = inData[j], inData[i]
-}
-
-func (inData myChars) Len() int {
-        return len(inData)
-}
-
-func SortChars(inStr string) string {
-        lBytes := []byte(inStr)
-        sort.Sort(myChars(lBytes))
-        return string(lBytes)
-}
-
-func findAnagrams(s string, p string) []int {
-    lMap := make(map[string]string)
-    var lOut []int
-    lLenP := len(p)
-    lInputSort := SortChars(p)
-    for i:=0;i<len(s);i++ {
-        //slice i to lLenP
-        if (i+lLenP) > len(s) {
-            break
-        }
-        lSubStr := s[i:i+lLenP]
-        lSubSort := ""
-        //fmt.Println("lBuStr=", lSubStr)
-        if val, ok := lMap[lSubStr]; ok {
-            //we have already sorted this, get its value
-            lSubSort = val
-            
-        } else {
-            lSubSort = SortChars(lSubStr)
-            lMap[lSubStr] = lSubSort
-        }
-        
-        
-        if lSubSort == lInputSort {
-            lOut = append(lOut, i)
-        }
-        
+func findCharFreq(inp string) []int {
+    out := make([]int, 26)
+    for _, each := range inp {
+        out[each-'a'] += 1
     }
-    return lOut
+    return out
+}
+
+func isAnagram(temp, pFreq []int) bool {
+    for idx, freq := range temp {
+        if pFreq[idx] != freq { return false }
+    }
+    return true
+}
+
+// K is the length of p
+// N is the length of s
+func findAnagrams(s string, p string) []int {
+    if len(s) < len(p) { return []int{} }
+    pFreq := findCharFreq(p) // K is the length of p; K operations
+    out := []int{}
+    temp := findCharFreq(string(s[:len(p)])) // K Steps (once)
+    for i:= 0; i <= len(s)-len(p); i++ { // N-K Steps
+        if isAnagram(temp, pFreq) { // Checks for anagram K steps
+            out = append(out, i)
+        }
+		temp[s[i]-'a'] -= 1 // Constant update
+		if i + len(p) >= len(s) { //constant check
+			continue
+		}
+		temp[s[i+len(p)]-'a'] += 1 //Constant update
+    }
+    return out
 }
