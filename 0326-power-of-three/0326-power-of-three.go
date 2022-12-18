4847326850eb69ed34b1ef0abff42a5906f4b3c7
Aman <33025632+theamanraj@users.noreply.github.com>
2022-12-18 07:57:52 +0530

    Time: 75 ms (17.12%), Space: 6.7 MB (8.95%) - LeetHub

---
+++ 0326-power-of-three/0326-power-of-three.go
@@ -0,0 +1,16 @@
+func isPowerOfThree(n int) bool {
+	s := strconv.FormatInt(int64(n), 3)
+	if s == "1" {
+		return true
+	}
+	if s[0] != '1' {
+		return false
+	}
+
+	for _, ch := range s[1:] {
+		if ch != '0' {
+			return false
+		}
+	}
+	return true
+}
