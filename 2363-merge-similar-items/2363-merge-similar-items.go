338bd2e549428dbda27a75b69d9b2dc82b121b3a
Aman <33025632+theamanraj@users.noreply.github.com>
2022-12-18 11:46:24 +0530

    Time: 15 ms (96.67%), Space: 6.6 MB (86.67%) - LeetHub

--- 2363-merge-similar-items/2363-merge-similar-items.go
+++ 2363-merge-similar-items/2363-merge-similar-items.go
@@ -1,19 +1,24 @@
 func mergeSimilarItems(items1 [][]int, items2 [][]int) [][]int {
-items := map[int]int{}
-res := [][]int{}
-for _, v1 := range items1 {
-items[v1[0]] += v1[1]
-}
-for _, v1 := range items2 {
-items[v1[0]] += v1[1]
-}
-
-for val, weight := range items{
-    res = append(res, []int{val, weight})
-}
-sort.SliceStable(res, func(i, j int) bool {
-    return res[i][0] < res[j][0]
-})
-
-return res
+    m := make(map[int]int)
+    for _, items := range items1 {
+        if v, ok := m[items[0]]; ok {
+            m[items[0]] = v + items[1]
+        } else {
+            m[items[0]] = items[1]
+        }
+    }
+    for _, items := range items2 {
+        if v, ok := m[items[0]]; ok {
+            m[items[0]] = v + items[1]
+        } else {
+            m[items[0]] = items[1]
+        }
+    }
+    ans := make([][]int, 0, len(m))
+    for k, v := range m {
+        t := []int{k, v}
+        ans = append(ans, t)
+    }
+    sort.Slice(ans, func(i, j int) bool { return ans[i][0] < ans[j][0] })
+    return ans
 }
