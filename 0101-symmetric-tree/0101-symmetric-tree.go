eb27790a5aba31d3dbcc77e1a1d1e8a671125fb9
Aman <33025632+theamanraj@users.noreply.github.com>
2022-12-31 07:34:01 +0530

    Time: 4 ms (59.06%), Space: 2.8 MB (100.00%) - LeetHub

---
+++ 0101-symmetric-tree/0101-symmetric-tree.go
@@ -0,0 +1,21 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func isSymmetric(root *TreeNode) bool {
+    return isMirror(root, root)
+}
+
+func isMirror(t1 *TreeNode,t2 *TreeNode) bool{
+    if t1 == nil && t2 == nil  {
+        return true
+    }
+    if t1 == nil || t2 == nil {
+        return false
+    }
+    return (t1.Val == t2.Val) && isMirror(t1.Left, t2.Right) && isMirror(t1.Right, t2.Left);
+}
