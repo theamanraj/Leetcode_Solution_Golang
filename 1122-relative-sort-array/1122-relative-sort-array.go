835e1cb95ca169e78e31a37e978c6e8a5e2f5b8c
Aman <33025632+theamanraj@users.noreply.github.com>
2023-01-08 19:46:44 +0530

    Time: 2 ms (90.00%), Space: 2.2 MB (90.00%) - LeetHub

---
+++ 1122-relative-sort-array/1122-relative-sort-array.go
@@ -0,0 +1,23 @@
+func relativeSortArray(arr1 []int, arr2 []int) []int {
+    count := make([]int,1001)
+    result := make([]int, 0, len(arr1))
+
+    for _,num:=range arr1{
+        count[num]++
+    }
+    
+    for _,num:=range arr2{
+        for j:=0; j<count[num]; j++{
+            result=append(result,num)
+        }
+        count[num]=0
+    }
+    
+    for i := 0; i < len(count); i++ {
+        for j:=0; j<count[i]; j++{
+            result=append(result,i)
+        }
+	}
+
+    return result
+}
