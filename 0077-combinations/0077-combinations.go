d5e61b2602876ee832afd502213788797682c92f
Aman <33025632+theamanraj@users.noreply.github.com>
2023-08-01 21:38:23 +0530

    Time: 4 ms (97.05%), Space: 6.4 MB (70.04%) - LeetHub

---
+++ 0077-combinations/0077-combinations.go
@@ -0,0 +1,20 @@
+func combine(n int, k int) [][]int {
+    ans := [][]int{}
+    backtrack(n, k, 1, []int{}, &ans)
+	return ans
+}
+
+func backtrack(n, k, start int, arr []int, ans *[][]int) {
+    if len(arr) == k {
+        comb := make([]int, len(arr))
+        copy(comb, arr)
+        *ans = append(*ans, comb)
+        return
+    }
+	
+	 for i := start; i <= n-(k-len(arr))+1; i++ {
+        arr = append(arr, i)
+        backtrack(n, k, i+1, arr, ans)
+        arr = arr[:len(arr)-1]
+    }
+}
