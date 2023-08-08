aff77348acd0d6cba3bf5a7b7c3a2fd6ba90be87
Aman <33025632+theamanraj@users.noreply.github.com>
2023-08-08 22:14:54 +0530

    Time: 6 ms (51.61%), Space: 4 MB (99.19%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.go
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.go
@@ -1,26 +1,22 @@
 func reverseVowels(s string) string {
-    bytes := []byte(s)
-    i := 0
-    vowels := map[byte]bool{'a': true, 'e': true, 'i': true, 'o': true, 'u': true, 'A': true, 'E': true, 'O': true, 'I': true, 'U': true, }
-    j := len(bytes) - 1
-    for i < j{
-        for i < j{
-            if _, ok := vowels[bytes[i]]; !ok{
-                i += 1
-            } else{
-                break
-            }
-        }
-        for i < j{
-            if _, ok := vowels[bytes[j]]; !ok{
-                j -= 1
-            } else{
-                break
-            }
-        }
-        bytes[i], bytes[j] = bytes[j], bytes[i]
-        i += 1
-        j -= 1
-    }
-    return string(bytes)
+	vowels := "aeiouAEIOU"
+	sBytes := []byte(s)
+	
+	i, j := 0, len(s)-1
+	
+	for i < j {
+		for i < j && !strings.Contains(vowels, string(sBytes[i])) {
+			i++
+		}
+		
+		for i < j && !strings.Contains(vowels, string(sBytes[j])) {
+			j--
+		}
+		
+		sBytes[i], sBytes[j] = sBytes[j], sBytes[i]
+		i++
+		j--
+	}
+	
+	return string(sBytes)
 }
