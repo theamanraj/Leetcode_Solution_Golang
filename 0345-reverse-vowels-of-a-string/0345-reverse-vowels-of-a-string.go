98ce3e79e256468efa611970a925d9e65561839c
Aman <33025632+theamanraj@users.noreply.github.com>
2023-08-09 22:19:40 +0530

    Time: 31 ms (13.31%), Space: 4.7 MB (33.60%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.go
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.go
@@ -7,13 +7,16 @@ func reverseVowels(s string) string {
 	for i < j {
 		for i < j && !strings.Contains(vowels, string(sBytes[i])) {
 			i++
+            fmt.Println(i,"ith Attempt")
 		}
 		
 		for i < j && !strings.Contains(vowels, string(sBytes[j])) {
 			j--
-		}
+            fmt.Println(j,"jth Attempt")
+		} 
 		
 		sBytes[i], sBytes[j] = sBytes[j], sBytes[i]
+        fmt.Println("nth Attempt")
 		i++
 		j--
 	}
