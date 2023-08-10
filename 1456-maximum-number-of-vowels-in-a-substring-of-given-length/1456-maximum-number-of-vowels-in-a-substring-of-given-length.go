9cbf63a35b3ee3c8197945a135647aa7054275af
Aman <33025632+theamanraj@users.noreply.github.com>
2023-08-10 20:13:30 +0530

    Time: 8 ms (72.84%), Space: 4.8 MB (60.19%) - LeetHub

--- 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.go
+++ 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.go
@@ -1,15 +1,14 @@
 func maxVowels(s string, k int) int {
-    vowels := "aeiouAEIOU"
 	maxVowelCount := 0
 	currentVowelCount := 0
 	
 	for i := 0; i < len(s); i++ {
-		if i >= k && strings.Contains(vowels, string(s[i-k])) {
-			currentVowelCount--
+		if isVowel(s[i]) {
+			currentVowelCount++
 		}
 		
-		if strings.Contains(vowels, string(s[i])) {
-			currentVowelCount++
+		if i >= k && isVowel(s[i-k]) {
+			currentVowelCount--
 		}
 		
 		if currentVowelCount > maxVowelCount {
@@ -18,4 +17,9 @@ func maxVowels(s string, k int) int {
 	}
 	
 	return maxVowelCount
+}
+
+func isVowel(c byte) bool {
+	return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' ||
+		c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U'
 }
