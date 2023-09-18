8cfdacc2d8f587c8d0013cabb6997838080c2eb7
Aman <33025632+theamanraj@users.noreply.github.com>
2023-09-18 15:59:21 +0530

    Time: 0 ms (100.00%), Space: 2.3 MB (99.65%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.go
+++ 0088-merge-sorted-array/0088-merge-sorted-array.go
@@ -1,20 +1,23 @@
-func merge(nums1 []int, m int, nums2 []int, n int) {
-    i, j, k := m-1, n-1, m+n-1
+func merge(nums1 []int, m int, nums2 []int, n int)  {
+    i,j,l := m-1,n-1,m+n-1
     
-    for i >= 0 && j >= 0 {
+    //execute loop till last index reach one of array
+    for i >= 0 && j >= 0{
+        // validate which num is better nd add it in last
         if nums1[i] > nums2[j] {
-            nums1[k] = nums1[i]
+            nums1[l] = nums1[i]
             i--
         } else {
-            nums1[k] = nums2[j]
+            nums1[l] = nums2[j]
             j--
         }
-        k--
+        // reduce total size
+        l--
     }
     
-    for j >= 0 {
-        nums1[k] = nums2[j]
-        k--
+        for j >= 0 {
+        nums1[l] = nums2[j]
+        l--
         j--
     }
 }
