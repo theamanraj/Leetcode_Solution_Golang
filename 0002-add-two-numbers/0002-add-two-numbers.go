ec81c2acb4b9450fd65742ab553c6e30cdea079e
Aman <33025632+theamanraj@users.noreply.github.com>
2024-02-10 19:45:36 +0530

    Time: 3 ms (92.74%), Space: 4.3 MB (47.32%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.go
+++ 0002-add-two-numbers/0002-add-two-numbers.go
@@ -6,38 +6,30 @@
  * }
  */
 func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+    var temp = &ListNode{}
     
-    tempNode := &ListNode{}
-    curNode := tempNode
-    var carryOn,temp int
-    for l1 != nil || l2 != nil{
-         i1 , i2 :=0, 0   
-        if l1!=nil{            
-            i1=l1.Val
-        }  
-        if l2!=nil{            
-            i2=l2.Val
-        }  
-          lval := i1 + i2 + carryOn     
-        carryOn = lval/10
-        temp = lval%10
-        curNode.Next=&ListNode{
-            Val:temp,
-        }    
-        curNode = curNode.Next
-         fmt.Println("carryon & temp value is",carryOn,temp,tempNode )
-        if l1!=nil{
+    current := temp
+    carry := 0
+    for(l1 != nil || l2 != nil || carry > 0){
+        sum := 0
+        if (l1 != nil){
+           sum = l1.Val 
+        }
+        if (l2 != nil){
+           sum += l2.Val 
+        }
+        sum += carry
+        carry = sum/10                 
+        current.Next=&ListNode{
+            Val:sum%10,
+        }
+        current = current.Next
+        if (l1 != nil) {
             l1=l1.Next
         }
-        if l2!=nil {
+        if (l2 != nil) {
             l2=l2.Next
-        }  
+        }
     }
-    if carryOn >0{
-                curNode.Next=&ListNode{
-            Val:carryOn,
-        }    
-        curNode = curNode.Next
-    }
-    return tempNode.Next    
+    return temp.Next
 }
